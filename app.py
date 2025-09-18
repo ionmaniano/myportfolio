23ef0a0c40ebb48323f3f9a855af65a8198b66ad
ionmaniano <odiwuorian@gmail.com>
2025-09-18 18:07:54 +0300

    Deploy Python app to Heroku

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import pymysql
 app = Flask(__name__)
 
 
+connection=pymysql.connect(host='localhost',user='root',password='',database='myportfolio') #creating a connection to our database using function connect
+cursor=connection.cursor()
+app.secret_key="bazenga" #We are cerating secret keys to secure our sessions/make it unique
+
 # Home Route
 @app.route('/')
 def home():
@@ -27,11 +31,25 @@ def education():
 def project():
     return render_template('project.html')
 
-@app.route('/contacts.html')
+@app.route('/contacts', methods=['POST', 'GET'])
 def contacts():
-    
-    return render_template('contacts.html')
+        if request.method == 'POST':
+            Name = request.form['Name']
+            Email = request.form['Email']
+            Subject = request.form['Subject']
+            Message = request.form['Message']
 
+            
+            cursor = connection.cursor()
+
+            sql = "INSERT INTO contact_info (Name,Email,Subject, Message) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (Name,Email,Subject, Message))
+
+            connection.commit()
+            cursor.close()
+            connection.close()
+        
+        return render_template('contacts.html', message='Sent successfuly')
 # Run Flask App
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
